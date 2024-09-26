65501f07c424ea4cdae0fdd6c76e97518fdeac4f
code_Labsquire <code@labsquire.com>
2024-09-26 14:06:30 +0530

    KR -- get service api

--- src/routers/servicesRoutes.ts
+++ src/routers/servicesRoutes.ts
@@ -5,17 +5,18 @@ const servicesRouter = new Hono();
 const servicesController = new ServicesController();
 
 servicesRouter.get('/count', servicesController.getTotalServices);
-servicesRouter.get('/', servicesController.listServices);
+// servicesRouter.get('/', servicesController.listServices);
 servicesRouter.post('/', servicesController.addService);
 servicesRouter.patch('/:id', servicesController.updateService);
 servicesRouter.delete('/:id', servicesController.deleteService);
 servicesRouter.get('/dashboard/invoice-amount', servicesController.listServicesWiseInvoicesAmount);
 servicesRouter.get('/count', servicesController.getTotalServices);
 servicesRouter.get('/', servicesController.listServices);
-servicesRouter.post('/', servicesController.addService);
+// servicesRouter.post('/', servicesController.addService);
 servicesRouter.patch('/:id', servicesController.updateService);
 servicesRouter.delete('/:id', servicesController.deleteService);
 servicesRouter.get('/drop-down', servicesController.getlistServiceForDropDown);
+servicesRouter.get('/:id',servicesController.getService)
 
 servicesRouter.get('/recurring-type/invoice-count', servicesController.getInvoiceAmountCountForRecurringServiceType);
 servicesRouter.get('/one-time-type/invoice-count', servicesController.getInvoiceAmountCountForOneTimeServiceType);
