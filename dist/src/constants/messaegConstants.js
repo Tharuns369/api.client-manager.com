a91ab87fa7a9ed1cbab1c730cd2b40dc11d42d3d
code_Labsquire <code@labsquire.com>
2024-09-27 16:35:32 +0530

    KR -- updated invoice valid

--- dist/src/constants/messaegConstants.js
+++ dist/src/constants/messaegConstants.js
@@ -104,9 +104,9 @@ const CLIENT_VALIDATION_MESSAGES = {
     COMPANY_NAME_VALIDATIONAS: "POC must only contain alphabets and spaces",
 };
 const INVOICE_VALIDATION_MESSAGES = {
-    SERVICE_ID_REQUIRED: "Service id is required.",
+    SERVICE_ID_REQUIRED: "Service is required.",
     SERVICE_ID_INVALID: "Service Id must be a positive integer.",
-    CLIENT_ID_REQUIRED: "Client Id is required.",
+    CLIENT_ID_REQUIRED: "Client is required.",
     CLIENT_ID_INVALID: "Client Id must be a positive integer.",
     INVALID_INVOICE_STATUS: "Invalid invoice status.",
     REMARKS_INVALID: "Invalid remarks format.",
