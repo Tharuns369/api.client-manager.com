382fe24c60af3c296c4d303b254764eec8e9ed1f
code_Labsquire <code@labsquire.com>
2024-09-28 15:52:54 +0530

    KR -- updated valid

---
+++ src/validations/clientsValidations/updateClientValidations.ts
@@ -0,0 +1,41 @@
+import * as v from 'valibot';
+import { CLIENT_VALIDATION_MESSAGES } from '../../constants/messaegConstants';
+
+const alphaRegex = /^[a-zA-Z\s]+$/;
+const phoneRegex = /^\+?[1-9]\d{0,2}[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{1,4}[-.\s]?\d{4,9}$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export enum StatusEnum {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+}
+
+export const clientUpdateValidationSchema = v.object({
+  client_name: v.pipe(
+    v.string(CLIENT_VALIDATION_MESSAGES.CLIENT_NAME_REQUIRED),
+    v.nonEmpty(CLIENT_VALIDATION_MESSAGES.CLIENT_NAME_REQUIRED),
+    v.regex(alphaRegex, CLIENT_VALIDATION_MESSAGES.CLIENT_NAME_INVALID)
+  ),
+  company_name: v.pipe(
+    v.string(CLIENT_VALIDATION_MESSAGES.COMPANY_NAME_REQUIRED),
+    v.nonEmpty(CLIENT_VALIDATION_MESSAGES.COMPANY_NAME_REQUIRED),
+    v.regex(alphaRegex, CLIENT_VALIDATION_MESSAGES.COMPANY_NAME_VALIDATIONAS)
+  ),
+  poc: v.pipe(
+    v.string(CLIENT_VALIDATION_MESSAGES.CLIENT_POC_REQUIRED),
+    v.nonEmpty(CLIENT_VALIDATION_MESSAGES.CLIENT_POC_REQUIRED),
+    v.regex(alphaRegex, CLIENT_VALIDATION_MESSAGES.CLIENT_POC_INVALID)
+  ),
+  email: v.pipe(
+    v.string(CLIENT_VALIDATION_MESSAGES.EMAIL_REQUIRED),
+    v.nonEmpty(CLIENT_VALIDATION_MESSAGES.EMAIL_REQUIRED),
+    v.regex(emailRegex, CLIENT_VALIDATION_MESSAGES.INVALID_EMAIL_FORMAT)
+  ),
+//   phone: v.pipe(
+//     v.string(CLIENT_VALIDATION_MESSAGES.PHONE_REQUIRED),
+//     v.nonEmpty(CLIENT_VALIDATION_MESSAGES.PHONE_REQUIRED),
+//     v.regex(phoneRegex, CLIENT_VALIDATION_MESSAGES.PHONE_INVALID)
+//   )
+});
+
+export type ClientUpdateValidationInput = v.InferInput<typeof clientUpdateValidationSchema>;
